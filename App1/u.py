2c1c30f5b51d211a5253a880d799eb680eeceeee
Mathiprakasam <kmathiprakasam@gmail.com>
2025-02-14 07:05:53 +0530

    Deleted u.py

--- App1/u.py
+++
@@ -1,8 +0,0 @@
-from faker import Faker
-
-fake = Faker()
-
-print(fake.name())       # Generates a random name
-print(fake.address())    # Generates a random address
-print(fake.email())      # Generates a random email
-print(fake.text())       # Generates random text
